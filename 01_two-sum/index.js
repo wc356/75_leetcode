0a8d14e18d45ba0ea2e3a661cc7201d6e7f1e00d
Woo <wychoi356@gmail.com>
2021-07-30 22:04:40 -0400

    add best_time_to_buy_n_sell_stocks

--- 01_two-sum/index.js
+++ 01_two-sum/index.js
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/two-sum/
+//leetcode.com/problems/two-sum/
 /**
  * 1. Two Sum
  * Easy
@@ -6,32 +6,32 @@ https://leetcode.com/problems/two-sum/
  * Add to List
  *
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- * 
+ *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * 
+ *
  * You can return the answer in any order.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: nums = [2,7,11,15], target = 9
  * Output: [0,1]
  * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
  * Example 2:
- * 
+ *
  * Input: nums = [3,2,4], target = 6
  * Output: [1,2]
  * Example 3:
- * 
+ *
  * Input: nums = [3,3], target = 6
  * Output: [0,1]
- * 
+ *
  * Constraints:
- * 
+ *
  * 2 <= nums.length <= 104
  * -109 <= nums[i] <= 109
  * -109 <= target <= 109
  * Only one valid answer exists.
- *  
+ *
  * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
  */
 
@@ -45,7 +45,6 @@ https://leetcode.com/problems/two-sum/
 
 // map = { 2: 0, 7: 1 }
 
-
 // [2,7,11,15]
 
 // [-5, -3, -2, 0, 2, 5, 6, 12], 7
@@ -53,17 +52,17 @@ https://leetcode.com/problems/two-sum/
 
 // HCL
 
-var twoSum = function(nums, target) {
-    const map = {};
-    
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i];
-        if (map[diff] !== undefined) {
-            return [i, map[diff]];
-        } 
-        map[nums[i]] = i;
-    }
-};
+// var twoSum = function (nums, target) {
+//   const map = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let diff = target - nums[i];
+//     if (map[diff] !== undefined) {
+//       return [i, map[diff]];
+//     }
+//     map[nums[i]] = i;
+//   }
+// };
 
 // input: array, int
 // output: [idx1, idx2]
@@ -91,5 +90,24 @@ var twoSum = function(nums, target) {
 //   }
 // }
 
+// function twoSum(ar, target) {
+//   for (let i = 0; i < ar.length; i++) {
+//     for (let j = i + 1; j < ar.length; j++) {
+//       if (ar[i] + ar[j] === target) return [i, j];
+//     }
+//   }
+// }
+
+function twoSum(ar, target) {
+  const map = {};
+  for (let i = 0; i < ar.length; i++) {
+    let diff = target - ar[i];
+    if (map[diff] !== undefined) {
+      return [i, map[diff]];
+    }
+    map[ar[i]] = i;
+  }
+}
+
 console.log(twoSum([3, 2, 4], 6));
-console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([2, 7, 11, 15], 9));
---
+++ 02_best_time_to_buy_n_sell_stock/index.js
@@ -0,0 +1,97 @@
+/**
+ * Best Time to Buy and Sell Stock
+ * Easy
+ *
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ *
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ *
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ * Example 2:
+ *
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transactions are done and the max profit = 0.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= prices.length <= 105
+ * 0 <= prices[i] <= 104
+ **/
+
+// var maxProfit = function (prices) {
+//   let max = 0;
+
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j = 0; j < prices.length; j++) {
+//       if (j > i && prices[j] - prices[i] > max) {
+//         max = prices[j] - prices[i];
+//       }
+//     }
+//   }
+
+//   return max;
+// };
+
+// var maxProfit = function (prices) {
+//   let min = 10000;
+//   let max_profit = 0;
+
+//   for (let i = 0; i < prices.length; i++) {
+//     if (prices[i] < min) {
+//       min = prices[i];
+//     } else if (prices[i] - min > max_profit) {
+//       max_profit = prices[i] - min;
+//     }
+//   }
+
+//   return max_profit;
+// };
+
+function maxProfit(prices) {
+  if (prices.length < 2) return 0;
+
+  let min = prices[0];
+  let max = prices[prices.length - 1];
+  let p2 = prices.length - 1;
+
+  for (let p1 = 0; p1 < Math.floor(prices.length / 2); p1++) {
+    if (prices[p1] < min) {
+      min = prices[p1];
+    }
+    if (prices[p2] > max) {
+      max = prices[p2];
+    }
+    p2--;
+  }
+
+  if (max < min) {
+    return 0;
+  } else if (prices.length % 2 !== 0) {
+    if (
+      prices[Math.floor(prices.length / 2)] > max &&
+      prices[Math.floor(prices.length / 2)] > min
+    ) {
+      return prices[Math.floor(prices.length / 2)] - min;
+    } else if (prices[Math.floor(prices.length / 2)] < min) {
+      return max - prices[Math.floor(prices.length / 2)];
+    }
+  } else {
+    return max - min;
+  }
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
+console.log(maxProfit([1, 4, 2]));
+console.log(maxProfit([1, 2, 4]));
